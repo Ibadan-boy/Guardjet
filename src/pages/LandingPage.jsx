79964c0b9e0751517aee6efe70a4dc0632f431ab
Ibadan-boy <samuelmakindepaul@gmail.com>
2025-08-26 21:29:32 +0100

    added backend and some new components

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -22,6 +22,12 @@ export default function LandingPage(){
           imageSrc={Headset}
           ctaText="Shop Now"
           subtitle="Experience effortless and secure transactions right from your beloved gadgets with our cutting-edge payment solutions. Designed to integrate seamlessly with your devices, our technology ensures fast, reliable purchases anytime, anywhere. Enjoy a hassle-free shopping journey tailored to enhance your gadget experience."/>
+          <Card
+          title="Your Best Gadget Showroom"
+          imageSrc={Headset}
+          ctaText="See Products"
+          invert={true}
+          subtitle="I initialized the project in my console, having watched movies to my satisfaction. I also found a Figma template (mobile version) that I want to use for the project; I'll build on that to expand it into the web version. Tonight, I'll draft the web version on paper."/>
           <Footer />
         </>
     )
