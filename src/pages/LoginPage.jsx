7433a3f74110bbdeb3c1bbaa6c4da0cba56390d4
Ibadan-boy <samuelmakindepaul@gmail.com>
2025-08-27 16:11:36 +0100

    styled the login and shop pages

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,11 @@
 export default function LoginPage(){
     return(
         <>
-        <h1>Login Page</h1>
-        <p>Login Page under construction</p>
+        <div className="container mx-auto p-4 min-h-screen flex flex-col items-center justify-center">
+         <h1 className="text-3xl font-bold mb-4">Login Page</h1>
+         <p className="text-lg text-gray-600">Login Page under construction</p>
+        </div>
+        
         </>
     )
 }
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,8 +1,10 @@
 export default function Shop(){
     return(
         <>
-        <h1>Shop Page</h1>
-        <p>Shop Page under construction</p>
+        <div className="container mx-auto p-4 min-h-screen flex flex-col items-center justify-center">
+            <h1 className="text-3xl font-bold mb-4">Shop Page</h1>
+            <p className="text-lg text-gray-600">Shop Page under construction</p>
+        </div>
         </>
     )
 }
